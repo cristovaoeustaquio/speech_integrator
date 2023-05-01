48fa3eb1923d77a2cb44c2a806b410351083d1fe
Elias Faria <98eliasfaria@gmail.com>
2023-05-01 20:49:49 -0300

    Deixando botão de play funcional e cards de mensagens dinamicos

--- frontend/src/Components/Messages/Messages.js
+++ frontend/src/Components/Messages/Messages.js
@@ -1,21 +1,53 @@
 import "./Messages.css";
-
+import { useState, useRef } from "react";
 import play from "../../Images/play.png";
+import audioQuestion from "../../Audios/recorded_audio2.wav";
+import audioAwnser from "../../Audios/output.wav";
 
+export default function Messages(props) {
+  const [isPlayingQuestion, setIsPlayingQuestion] = useState(false);
+  const [isPlayingAnwser, setIsPlayingAnwser] = useState(false);
+  const audioRefQ = useRef(null);
+  const audioRefA = useRef(null);
 
-export default function Messages() {
+  function handleClickQuestion() {
+    setIsPlayingQuestion(!isPlayingQuestion);
+    if (!isPlayingQuestion) {
+      audioRefQ.current.play();
+    } else {
+      audioRefQ.current.pause();
+    }
+  }
+
+  function handleClickAnwser() {
+    setIsPlayingAnwser(!isPlayingAnwser);
+    if (!isPlayingAnwser) {
+      audioRefA.current.play();
+    } else {
+      audioRefA.current.pause();
+    }
+  }
+
   return (
     <div className="messages">
-    <div className="question_content">
-      <div className="question">
-      <img src={play}></img>
+      <div className="question_content">
+        <div className="question">
+          <div className="question_play">
+            <img src={play} onClick={handleClickQuestion}></img>
+            <audio ref={audioRefQ} src={audioQuestion} />
+          </div>
+          <div className="question_text">{props.question}</div>
+        </div>
+      </div>
+      <div className="anwser_content">
+        <div className="anwser">
+          <div className="anwser_play">
+            <img src={play} onClick={handleClickAnwser}></img>
+            <audio ref={audioRefA} src={audioAwnser} />
+          </div>
+          <div className="anwser_text">{props.anwser}</div>
+        </div>
       </div>
     </div>
-    <div className="anwser_content">
-      <div className="anwser">
-      <img src={play}></img>
-      </div>
-    </div>
-  </div>
   );
 }
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -7,6 +7,14 @@ import Messages from "../../Components/Messages/Messages.js";
 import { useNavigate } from "react-router-dom";
 import mic from "../../Images/Vector.png";
 
+const dados = [
+  { question: "Hello Chat GPT!", anwser: "Hello Human!" },
+  { question: "Question 1", anwser: "Anwser 1" },
+  { question: "Question 2", anwser: "Anwser 2" },
+  { question: "Question 3", anwser: "Anwser 3" },
+  { question: "Question 4", anwser: "Anwser 4"}
+];
+
 function Home() {
   return (
     <div className="container">
@@ -17,12 +25,12 @@ function Home() {
           <div className="sair_button">Sair</div>
         </div>
         <div className="history">
-          <div className="messages_content">      
-          <Messages />  
-          <Messages />
-          <Messages />
-          <Messages />
-          <Messages />
+          <div className="messages_content">
+            {dados.map((linha, index) => (
+              <tr key={index}>
+                <Messages question={linha.question} anwser={linha.anwser} />
+              </tr>
+            ))}
           </div>
           <div className="talk_button">
             <div className="mic">
