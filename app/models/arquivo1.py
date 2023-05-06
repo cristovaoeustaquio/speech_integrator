4b9d2d4893e96dd1c250df7456816526c49c6271
Luiz Otávio A. Gonçalves <48141365+Goncalui@users.noreply.github.com>
2023-05-06 20:36:25 -0300

    RemoveAll

--- app/models/arquivo1.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class ClasseModelo1(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
-
-    def __repr__(self):
-        return f'ClasseModelo1(id={self.id}, nome={self.nome}, email={self.email})'
--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'minha_chave_secreta'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///meu_banco_de_dados.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
